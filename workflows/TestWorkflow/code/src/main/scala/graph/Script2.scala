f66c3d5f6718c84e09feb02b1c0dcb81f3e0088c
George <george@sdfd.com>
2021-08-20 19:18:33 +0000

    New Commit for Workflow Code

--- workflows/TestWorkflow/code/src/main/scala/graph/Script2.scala
+++ workflows/TestWorkflow/code/src/main/scala/graph/Script2.scala
@@ -10,7 +10,7 @@ import udfs.UDFs._
 import udfs._
 import graph._
 
-@Visual(id="Script2", label="Script2", x = 1328, y = 126, phase = 0)
+@Visual(id="Script2", label="Script2", x = 845, y = 462, phase = 0)
 object Script2 {
   
   def apply(spark: SparkSession): ScriptUnit = {
